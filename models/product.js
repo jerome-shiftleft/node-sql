14b23765c22ea2d730aad2a2c9fad00887e3b6e1
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-29 11:21:43 +0800

    Refactor Product model to use database module

--- models/product.js
+++ models/product.js
@@ -1,3 +1,5 @@
+const db = require("../util/database");
+
 const Cart = require("./cart");
 
 module.exports = class Product {
@@ -9,19 +11,13 @@ module.exports = class Product {
     this.price = price;
   }
 
-  save() {
-    
+  save() {}
+
+  static deleteById(id) {}
+
+  static fetchAll() {
+    return db.execute("SELECT * FROM products");
   }
 
-  static deleteById(id) {
-    
-  }
-
-  static fetchAll(cb) {
-    
-  }
-
-  static findById(id, cb) {
-   
-  }
+  static findById(id) {}
 };
